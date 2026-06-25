a6e96d9c12a0f5d1c1a6759144380a51b803ddfd
Felix Gateru <felix.gateru@gmail.com>
2026-06-25 18:45:23 +0300

    feat: initialize Spring Boot project with Maven wrapper
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

---
+++ src/main/java/org/wanyoh/gawa/GawaApplication.java
@@ -0,0 +1,13 @@
+package org.wanyoh.gawa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GawaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GawaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wanyoh/gawa/GawaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wanyoh.gawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
